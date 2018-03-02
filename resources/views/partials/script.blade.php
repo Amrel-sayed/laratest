02446d47c33eca4580dac148ab0799bb87f46b09
Mohamed-ElSayed <ASayed185@vodafone.com>
2018-03-02 10:41:12 +0200

    new laravel project

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1,15 +1,15 @@
-		<script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+		<script src="{{ secure_asset('js/jquery-3.2.1.min.js') }}"></script>
 
-        <script src="{{ asset('js/popper.min.js') }}"></script> 
+        <script src="{{ secure_asset('js/popper.min.js') }}"></script> 
         <script src="http://maps.google.com/maps/api/js?sensor=true"></script>     
-        {{-- <script src="{{ asset('js/gmaps.min.js') }}"></script> --}}
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        <script defer src="{{ asset('js/fontawesome-all.min.js') }}"></script>
-        <script type="text/javascript" src="{{ asset('js/jquery.plugin.js') }}"></script>	
-<!--        <script type="text/javascript" src="{{ asset('js/flip.js') }}"></script>	-->
-        <script type="text/javascript" src="{{ asset('js/jquery.countdown.js') }}"></script>
-        <script src="{{ asset('js/sweet2.min.js') }}"></script>
-        <script src="{{ asset('js/sweet2.core.js') }}"></script>
-        <script src="{{ asset('js/jquery.fancybox.js') }}"></script>
+        {{-- <script src="{{ secure_asset('js/gmaps.min.js') }}"></script> --}}
+        <script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
+        <script defer src="{{ secure_asset('js/fontawesome-all.min.js') }}"></script>
+        <script type="text/javascript" src="{{ secure_asset('js/jquery.plugin.js') }}"></script>	
+<!--        <script type="text/javascript" src="{{ secure_asset('js/flip.js') }}"></script>	-->
+        <script type="text/javascript" src="{{ secure_asset('js/jquery.countdown.js') }}"></script>
+        <script src="{{ secure_asset('js/sweet2.min.js') }}"></script>
+        <script src="{{ secure_asset('js/sweet2.core.js') }}"></script>
+        <script src="{{ secure_asset('js/jquery.fancybox.js') }}"></script>
        
-        <script src="{{ asset('js/myjs.js') }}"></script>
+        <script src="{{ secure_asset('js/myjs.js') }}"></script>
